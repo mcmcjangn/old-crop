5997d281f973e665a71068450f149750e46e17cb
LGY010011 <gylm1111pm@gmail.com>
2024-05-09 00:50:33 +0900

    각 stage 크리쳐 헤더파일 작성

---
+++ Folktale/Folktale/Creature_stage3.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Creature.h"
+
+
+//까치
+class Magpie : public Ally //까치
+{
+private:
+public:
+	Magpie(int _health, int _x, int _y);
+};
+
+//구렁이
+class Snake : public Monster  //구렁이
+{
+private:
+
+public:
+
+};
+
